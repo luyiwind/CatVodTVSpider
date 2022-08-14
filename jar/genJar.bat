4fe22e0c70bc6717e04542172505d13a27c1b722
luyiwind <70989874+luyiwind@users.noreply.github.com>
2022-08-15 07:39:30 +0800

    Update genJar.bat

--- jar/genJar.bat
+++ jar/genJar.bat
@@ -1,6 +1,6 @@
 @echo off
 
-del "%~dp0\custom_spider.jar"
+del "%~dp0\custom_spider_xp.jar"
 rd /s/q "%~dp0\Smali_classes"
 
 java -jar "%~dp0\3rd\baksmali-2.5.2.jar" d "%~dp0\..\app\build\intermediates\dex\release\minifyReleaseWithR8\classes.dex" -o "%~dp0\Smali_classes"
@@ -21,9 +21,9 @@ rd /s/q "%~dp0\Smali_classes"
 
 java -jar "%~dp0\3rd\apktool_2.4.1.jar" b "%~dp0\spider.jar" -c
 
-move "%~dp0\spider.jar\dist\dex.jar" "%~dp0\custom_spider.jar"
+move "%~dp0\spider.jar\dist\dex.jar" "%~dp0\custom_spider_xp.jar"
 
-certUtil -hashfile "%~dp0\custom_spider.jar" MD5 | find /i /v "md5" | find /i /v "certutil" > "%~dp0\custom_spider.jar.md5"
+certUtil -hashfile "%~dp0\custom_spider_xp.jar" MD5 | find /i /v "md5" | find /i /v "certutil" > "%~dp0\custom_spider_xp.jar.md5"
 
 rd /s/q "%~dp0\spider.jar\smali\com\github\catvod\spider"
 rd /s/q "%~dp0\spider.jar\smali\com\github\catvod\parser"
