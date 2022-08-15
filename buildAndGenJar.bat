59a41451f2d41a74d1a0aacc10dbe7d4f65e700c
luyiwind <70989874+luyiwind@users.noreply.github.com>
2022-08-15 11:12:53 +0800

    Update buildAndGenJar.bat

--- buildAndGenJar.bat
+++ buildAndGenJar.bat
@@ -1,7 +1,11 @@
 @echo off
 
 call "%~dp0\gradlew" assembleRelease --no-daemon
-
+IF %ERRORLEVEL% NEQ 0 ( 
+   exit 1 
+)
 call "%~dp0\jar\genJar.bat" %1
-
+IF %ERRORLEVEL% NEQ 0 ( 
+   exit 1 
+)
 pause
